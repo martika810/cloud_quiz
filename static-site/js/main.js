0e72e9424e0cc797455701457c90e0cd5b3f4c55
1984MRB <fraguel_rock22@hotmail.com>
2019-09-03 15:28:59 +0100

    Update main.js

---
+++ static-site/js/main.js
@@ -0,0 +1,10 @@
+$.ajax({
+		type:"GET",
+		ur:"https://kyw8h96294.execute-api.eu-west-1.amazonaws.com/aws_questions_stage",
+		crossDomain: true,
+		contentType: "application/json",
+		success: function(data,status){
+			console.log('Request to the api');
+			console.log(JSON.stringify(data.question));
+		}
+});
