dc34532c0ae548c5356b274c75f5c33ef88acc7d
1984MRB <fraguel_rock22@hotmail.com>
2019-09-03 17:05:25 +0100

    Update main.js

--- static-site/js/main.js
+++ static-site/js/main.js
@@ -3,8 +3,17 @@ $.ajax({
 		ur:"https://kyw8h96294.execute-api.eu-west-1.amazonaws.com/aws_questions_stage/",
 		crossDomain: true,
 		contentType: "application/json",
+		beforeSend: function(xhr){
+			xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+			xhr.setRequestHeader("Access-Control-Allow-Methods", "OPTIONS,POST");
+			xhr.setRequestHeader("Access-Control-Allow-Headers", "X-Requested-With,Access-Control-Allow-Headers,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token");
+			xhr.setRequestHeader("Content-Type", "application/json");
+		},
 		success: function(body,statusCode){
 			console.log('Request to the api');
 			console.log(JSON.stringify(body));
+		},
+		error: function(data){
+			console.log("error = "+JSON.stringify(data));
 		}
 });
